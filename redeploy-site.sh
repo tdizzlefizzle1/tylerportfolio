88615b4f1b40819bbcec961c4eba6a1ad8a9c928
Tyler H <80732627+tdizzlefizzle1@users.noreply.github.com>
2022-06-20 00:05:58 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,5 +5,4 @@ cd ~/tylerportfolio/
 git fetch && git reset origin/main --hard
 source .venv/bin/activate
 pip install -r requirements.txt
-#figure out how to get into the virtual environment before we run the server
 tmux new-session -d -s 0 'source .venv/bin/activate && flask run --host=0.0.0.0'
