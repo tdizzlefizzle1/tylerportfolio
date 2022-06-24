fafa8af7d9bd339f6bcf1c684049d53f27fa3eb1
tdizzlefizzle1 <tylerhols@yahoo.com>
2022-06-24 20:42:55 +0000

    removed redeployment script

--- redeploy-site.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tmux kill-server
-cd ~/tylerportfolio/
-git fetch && git reset origin/main --hard
-source .venv/bin/activate
-pip install -r requirements.txt
-tmux new-session -d -s 0 'source .venv/bin/activate && flask run --host=0.0.0.0'
